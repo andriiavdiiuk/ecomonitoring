ba75bfb9cdbf5e30ba9a29fa37ff38f0ef993c61
Andrii <andriy07da@gmail.com>
2025-09-18 16:13:07 +0300

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+    eslint.configs.recommended,
+    tseslint.configs.strictTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+);
