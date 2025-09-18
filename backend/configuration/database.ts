14de3fc53305e5a744cbe2994b874c10fbc657d6
Andrii <andriy07da@gmail.com>
2025-09-18 13:30:19 +0300

    add database

---
+++ backend/configuration/database.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+import config from './config';
+async function connectDB () {
+    try {
+        const conn = await mongoose.connect(config.db_connection_string);
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(`Database: ${conn.connection.name}`);
+    } catch (error: any) {
+        console.error('MongoDB connection error:', error.message);
+        process.exit(1);
+    }
+}
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected from MongoDB');
+});
+
+process.on('SIGINT', async () => {
+    await mongoose.connection.close();
+    console.log('MongoDB connection closed due to app termination');
+    process.exit(0);
+});
+
+
+export default connectDB;
